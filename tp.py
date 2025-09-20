1e6f75ab5eff73b2727ad83c5ebf61679c5b8194
moku <nikitamillionov@gmail.com>
2025-09-20 21:43:46 +0300

    добавленна операция

--- tp.py
+++ tp.py
@@ -1,3 +1,4 @@
 num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второе число: "))
-print(f"Вы ввели: {num1} и {num2}")
+result = num1 + num2
+print(f"Результат сложения: {num1} + {num2} = {result}")
