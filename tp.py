42ac6c5f51e57a65b3350204d74cb858105e632c
mokundra <nikitamillionov@gmail.com>
2025-09-20 21:48:01 +0300

    tp.py изменил операцию

--- tp.py
+++ tp.py
@@ -1,4 +1,4 @@
 num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второе число: "))
-result = num1 + num2
-print(f"Результат сложения: {num1} + {num2} = {result}")
+result = num1 - num2
+print(f"Результат сложения: {num1} - {num2} = {result}")
